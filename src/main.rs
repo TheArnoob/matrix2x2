eca190661d1d083f435d90c7a9e4f99e4a5182b9
Arnold <arnold@afach.de>
2024-03-23 12:51:00 +0400

    Add some examples.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use matrix::Matrix2x2;
 mod matrix;
 
-fn main(){
+fn main() {
     let mat_a = Matrix2x2::new(6., 4., 3., 5.);
     let mat_b = Matrix2x2::new(4., 2., 1., 3.);
     let mat_c = Matrix2x2::new(5., 3., 4., 6.);
@@ -12,7 +12,11 @@ fn main(){
     println!("{}", mat_a.mul(&mat_b));
     println!("{}", mat_a.get(1, 1).unwrap());
     println!("Determinant of matrix a: {}", mat_a.det());
+    println!("Inverse of matrix a: {}", mat_a.inv());
+    println!("Determinant of matrix b: {}", mat_b.det());
     println!("Inverse of matrix b: {}", mat_b.inv());
     println!("Determinant of matrix c: {}", mat_c.det());
+    println!("Inverse of matrix c: {}", mat_c.inv());
+    println!("Determinent of matrix d: {}", mat_d.det());
     println!("Inverse of matrix d: {}", mat_d.inv());
 }
